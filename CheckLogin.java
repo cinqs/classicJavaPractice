3a5fb06d871c6c7fadd94351d54b26105c4039c6
jichangfeng67 <jichangfeng67@gmail.com>
2016-02-28 16:45:01 +0100

    a

--- CheckLogin.java
+++ CheckLogin.java
@@ -1,3 +1,4 @@
+package pkgClass;
 import java.util.Scanner;
 
 public class CheckLogin {
--- DistributeDepartment.java
+++ DistributeDepartment.java
@@ -1,10 +1,21 @@
-package pkgClass;
-
 import java.util.Scanner;
 
-class DistributeDepartment{
+public class DistributeDepartment{
   public static void main(String [] args){
     Scanner scan = new Scanner(System.in);
-    
+    System.out.println("input a name for the employee: ");
+    String name = scan.nextLine();
+    System.out.println("input the correspondent language: ");
+    String lang = scan.nextLine();
+    switch(lang){
+      case "java":
+        System.out.println("Voila, nous sommes une java");
+        break;
+      case "c++":
+        System.out.println("Voila, c'est C++");
+        break;
+      default:
+        System.out.println("c'est quoi?");
+    }
   }
 }
